3cd33284c594782bb391d917da0f09f9b1372e4e
rcarrata <rcarratalasanchez@gmail.com>
2023-09-06 15:44:38 +0200

    added gitops hierarchy

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Deploy OpenDataHub, Nvidia GPU Operator and Node Feature Discovery using GitOps!
 
 Repository for provision OpenDataHub, Nvidia GPU Operator and Node Feature Discovery using GitOps
+
+# Install OpenShift GitOps
+
+```sh
+oc apply -k https://github.com/redhat-cop/gitops-catalog/openshift-gitops-operator/operator/overlays/latest
+```
+
+## Install Operators
+
+```sh
+
+```
+
+## Deploy Operator instances
+
+```sh
+
+```
--- templates/deploy_rosa/tf/gpu-machinepool.tf
+++ templates/deploy_rosa/tf/gpu-machinepool.tf
@@ -1,7 +1,7 @@
 resource "rhcs_machine_pool" "machine_pool" {
-  cluster             = rhcs_cluster_rosa_classic.rosa.id
-  name                = "nvidia-gpu-pool"
-  machine_type        = var.machine_type
-  replicas            = var.machineset_replicas
-  autoscaling_enabled = var.autoscaling_enabled
+  cluster                 = rhcs_cluster_rosa_classic.rosa.id
+  name                    = "nvidia-gpu-pool"
+  machine_type            = var.machine_type
+  # use_spot_instances      = true
+  # multi_availability_zone = false
 }
